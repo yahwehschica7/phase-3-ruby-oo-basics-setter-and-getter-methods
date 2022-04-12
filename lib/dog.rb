d95579dab8195c674e9ed7e0d81b441b89c39bd3
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-12 14:44:40 -0400

    commit tree abc80ff30bfc06b7c8dc35842b13128095b86873

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+    attr_accessor :name 
+    attr_accessor :breed
+
+    def name=(name)
+        @name = name
+    end 
+
+    def breed=(breed)
+        @breed = breed 
+    end     
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
+    attr_accessor :name
+    attr_accessor :job 
+
+    def name=(name)
+        @name = name 
+    end
+
+    def job=(job)
+        @job = job
+    end 
 
 end
